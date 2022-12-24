f27f1de5aeb21f2a7edfeb4991147623f4fd9759
wmadison <wmadison@student.21-school.ru>
2022-12-24 16:24:05 +0500

    addition started aplications blog and projects

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -1,4 +1,4 @@
-"""service_test URL Configuration
+"""personal_portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("projects/", include("projects.urls")),
+    path("blog/", include("blog.urls")),
 ]
